1b654746bf160084b99b8724387f2da579264a04
Developer-Rathod <127027582+MarkusLaine19@users.noreply.github.com>
2024-10-11 04:11:37 +0300

    Labwork-3

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyMovieapp',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,83 @@
+import useApi, { SearchResult, SearchType } from '../hooks/useApi';
+import { IonAvatar, IonContent, IonHeader, IonIcon, IonImg, IonItem, IonLabel, IonList, IonPage, IonSearchbar, IonSelect, IonSelectOption, IonTitle, IonToolbar, useIonAlert, useIonLoading } from '@ionic/react';
+import { useEffect, useState } from 'react'; 
+import { gameControllerOutline, tvOutline, videocamOutline } from 'ionicons/icons'
+
+const Home: React.FC = () => {
+
+  const { searchData } = useApi()
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [type, setType] = useState<SearchType>(SearchType.all)
+  const [results, setResults] = useState<SearchResult[]>([])
+  const[presentAlert] = useIonAlert()
+  const [loading, dismiss] = useIonLoading()
+
+  useEffect(() => {
+    if (searchTerm === '') {
+      setResults([]); // If searchTerm is empty, clear results
+      return;
+    }
+  
+    const loadData = async () => {
+      await loading();
+  
+      const result: any = await searchData(searchTerm, type);
+      console.log("🚀 ~ file: Home.tsx:31 ~ loadData ~ result", result);
+  
+      await dismiss();
+  
+      // Check if the result contains an error
+      if (result?.Error) {
+        presentAlert(result.Error);
+        setResults([]); // Set empty array on error
+      } else if (result?.Search) {
+        setResults(result.Search); // Set search results if available
+      } else {
+        setResults([]); // Set empty array if no results
+      }
+    };
+  
+    loadData();
+  }, [searchTerm, type]);
+  
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color={'primary'}>
+          <IonTitle>My Movie App</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonSearchbar value={searchTerm}
+        debounce={300}
+        onIonChange={(e) => setSearchTerm(e.detail.value!)}></IonSearchbar>
+       <IonItem>
+        <IonLabel>Select Searchtype</IonLabel>
+        <IonSelect value={type} onIonChange={(e) => setType(e.detail.value!)}>
+          <IonSelectOption value ="">All</IonSelectOption>
+          <IonSelectOption value="movie">Movie</IonSelectOption>
+          <IonSelectOption value="series">series</IonSelectOption>
+          <IonSelectOption value="episode">Episode</IonSelectOption>
+        </IonSelect>
+        </IonItem> 
+        <IonList>
+          {results.map((item: SearchResult) => (
+            <IonItem button key={item.imdbID} routerLink={'/movies/${item.imdbID'}>
+              <IonAvatar slot='start'>
+                <IonImg src={item.Poster} />
+              </IonAvatar>
+              <IonLabel className='ion-text-wrap'>{item.Title}</IonLabel>
+              {item.Type === 'movie' && <IonIcon slot="end" icon={videocamOutline} />}
+              {item.Type === 'series' && <IonIcon slot="end" icon={tvOutline} />}
+              {item.Type === 'game' && <IonIcon slot="end" icon={gameControllerOutline} />}
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
